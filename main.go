39caf35a036b997962bccfdbd89e3845902fe9f3
elseym <elseym@me.com>
2017-06-02 19:17:40 +0200

    reformat go code

--- main.go
+++ main.go
@@ -1,120 +1,120 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net/http"
-	"os"
-	"strings"
-	"time"
+  "flag"
+  "fmt"
+  "net/http"
+  "os"
+  "strings"
+  "time"
 
-	"github.com/mdlayher/unifi"
+  "github.com/mdlayher/unifi"
 )
 
 type configuration struct {
-	api   string
-	user  string
-	pass  string
-	site  string
-	bind  string
-	base  string
-	wlist string
-	freq  time.Duration
+  api   string
+  user  string
+  pass  string
+  site  string
+  bind  string
+  base  string
+  wlist string
+  freq  time.Duration
 }
 
 var (
-	identifier = "mielke/1.0"
-	config     = configuration{}
+  identifier = "mielke/1.0"
+  config     = configuration{}
 )
 
 func main() {
-	var c *unifi.Client
-	var w *whitelist
-	var err error
+  var c *unifi.Client
+  var w *whitelist
+  var err error
 
-	fmt.Println(identifier + " initialising...")
+  fmt.Println(identifier + " initialising...")
 
-	if c, err = initClient(); err == nil {
-		if _, err = initSite(c); err == nil {
-			if w, err = NewWhitelist(c, config.wlist).Load(); err == nil {
-				go w.UpdateLoop(config.freq)
-				fmt.Println(identifier + " now serving...")
-				http.ListenAndServe(config.bind, w.Router())
-				os.Exit(0)
-			}
-		}
-	}
+  if c, err = initClient(); err == nil {
+    if _, err = initSite(c); err == nil {
+      if w, err = NewWhitelist(c, config.wlist).Load(); err == nil {
+        go w.UpdateLoop(config.freq)
+        fmt.Println(identifier + " now serving...")
+        http.ListenAndServe(config.bind, w.Router())
+        os.Exit(0)
+      }
+    }
+  }
 
-	fmt.Println("error: " + err.Error())
-	os.Exit(1)
+  fmt.Println("error: " + err.Error())
+  os.Exit(1)
 }
 
 // initClient connects and logs into the unifi endpoint
 func initClient() (c *unifi.Client, err error) {
-	c, err = unifi.NewClient(
-		config.api,
-		unifi.InsecureHTTPClient(4*time.Second),
-	)
+  c, err = unifi.NewClient(
+    config.api,
+    unifi.InsecureHTTPClient(4*time.Second),
+  )
 
-	if err == nil {
-		c.UserAgent = identifier
-		err = c.Login(config.user, config.pass)
-	}
+  if err == nil {
+    c.UserAgent = identifier
+    err = c.Login(config.user, config.pass)
+  }
 
-	return
+  return
 }
 
 // initSite checks, whether the configured site name exists
 func initSite(c *unifi.Client) (site *unifi.Site, err error) {
-	var sites []*unifi.Site
-	var siteNames []string
+  var sites []*unifi.Site
+  var siteNames []string
 
-	if sites, err = c.Sites(); err == nil {
-		for _, s := range sites {
-			siteNames = append(siteNames, s.Name)
-			if s.Name == config.site {
-				site = s
-				config.site = site.Name
-				break
-			}
-		}
+  if sites, err = c.Sites(); err == nil {
+    for _, s := range sites {
+      siteNames = append(siteNames, s.Name)
+      if s.Name == config.site {
+        site = s
+        config.site = site.Name
+        break
+      }
+    }
 
-		if site == nil {
-			err = fmt.Errorf(
-				"could not find site '%s' in ['%s'] on '%s'",
-				config.site,
-				strings.Join(siteNames, "','"),
-				config.api,
-			)
-		}
-	}
+    if site == nil {
+      err = fmt.Errorf(
+        "could not find site '%s' in ['%s'] on '%s'",
+        config.site,
+        strings.Join(siteNames, "','"),
+        config.api,
+      )
+    }
+  }
 
-	return
+  return
 }
 
 func init() {
-	mkFlag(&config.api, "api", "", "UniFi API")
-	mkFlag(&config.user, "user", "", "UniFi API Username")
-	mkFlag(&config.pass, "pass", "", "UniFi API Password")
-	mkFlag(&config.site, "site", "default", "UniFi Site")
-	mkFlag(&config.bind, "bind", ":5520", "HTTP Host:Port to bind to")
-	mkFlag(&config.base, "base", "", "Reverse Proxy Path Prefix")
-	mkFlag(&config.wlist, "whitelist", "whitelist.json", "List File")
-	mkFlag(&config.freq, "freq", "30s", "Update Frequency")
+  mkFlag(&config.api, "api", "", "UniFi API")
+  mkFlag(&config.user, "user", "", "UniFi API Username")
+  mkFlag(&config.pass, "pass", "", "UniFi API Password")
+  mkFlag(&config.site, "site", "default", "UniFi Site")
+  mkFlag(&config.bind, "bind", ":5520", "HTTP Host:Port to bind to")
+  mkFlag(&config.base, "base", "/", "Reverse Proxy Path Prefix")
+  mkFlag(&config.wlist, "whitelist", "whitelist.json", "List File")
+  mkFlag(&config.freq, "freq", "30s", "Update Frequency")
 
-	flag.Parse()
+  flag.Parse()
 }
 
 func mkFlag(v interface{}, name string, def string, desc string) {
-	val := os.Getenv("MIELKE_" + strings.ToUpper(name))
-	if val == "" {
-		val = def
-	}
-	switch v.(type) {
-	case *string:
-		flag.StringVar(v.(*string), name, val, desc)
-	case *time.Duration:
-		dur, _ := time.ParseDuration(val)
-		flag.DurationVar(v.(*time.Duration), name, dur, desc)
-	}
+  val := os.Getenv("MIELKE_" + strings.ToUpper(name))
+  if val == "" {
+    val = def
+  }
+  switch v.(type) {
+  case *string:
+    flag.StringVar(v.(*string), name, val, desc)
+  case *time.Duration:
+    dur, _ := time.ParseDuration(val)
+    flag.DurationVar(v.(*time.Duration), name, dur, desc)
+  }
 }
--- router.go
+++ router.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+  "net/http"
+  "strings"
 )
 
 type route struct {
-	path    string
-	method  string
-	headers map[string]string
-	handler http.HandlerFunc
+  path    string
+  method  string
+  headers map[string]string
+  handler http.HandlerFunc
 }
 
 type router struct {
-	routes []route
+  routes []route
 }
 
 func (t *router) add(path, method string, headers map[string]string, handler http.HandlerFunc) {
-	t.routes = append(t.routes, route{path, method, headers, handler})
+  t.routes = append(t.routes, route{path, method, headers, handler})
 }
 
 func (t router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for _, o := range t.routes {
-		matches := o.path == r.URL.Path && o.method == r.Method
-		for n, v := range o.headers {
-			matches = matches && strings.Contains(r.Header.Get(n), v)
-		}
-		if matches {
-			o.handler(w, r)
-			return
-		}
-	}
-	http.NotFound(w, r)
+  for _, o := range t.routes {
+    matches := o.path == r.URL.Path && o.method == r.Method
+    for n, v := range o.headers {
+      matches = matches && strings.Contains(r.Header.Get(n), v)
+    }
+    if matches {
+      o.handler(w, r)
+      return
+    }
+  }
+  http.NotFound(w, r)
 }
